7c9b495c931476da01a5b494e8ab3c7251f1341c
yangyuan <1492288361@qq.com>
2016-02-12 18:05:51 +0800

    modify

--- bin/commands/create.js
+++ bin/commands/create.js
@@ -3,37 +3,37 @@
  */
 
 var _   = require( 'underscore' ),
-	fs  = require( 'fs' ),
-	fse = require( 'fs-extra' );
+    fs  = require( 'fs' ),
+    fse = require( 'fs-extra' );
 
 function _help(){
-	console.log( 'Usage: jugg create <name of your blog>' );
+  console.log( 'Usage: jugg create <name of your blog>' );
 }
 
 exports.exec = function( options ){
- 	var name;
+  var name;
 
- 	if( options.length ){
- 		name = options[ 0 ];
- 	}
- 	// 如果参数数组是空或不是数组，那么打印 hlep 并退出程序
- 	else{
- 		_help();
- 		return;
- 	}
+  if( options.length ){
+    name = options[ 0 ];
+  }
+  // 如果参数数组是空或不是数组，那么打印 hlep 并退出程序
+  else{
+    _help();
+    return;
+  }
 
- 	var basic = __dirname + '/../../basic', // 基本结构模板
- 		src   = basic,
-		dest  = __dirname + '/../../local/' + name; // 生成博客的目标路径
+  var basic = __dirname + '/../../basic', // 基本结构模板
+      src   = basic,
+      dest  = __dirname + '/../../local/' + name; // 生成博客的目标路径
 
- 	if( fs.exists( basic, function( exists ){
- 		if( exists ){
- 			fse.copySync( src, dest );
- 			console.log( 'Blog ' + name + ' is created successfully.' );
- 		}else{
- 			// 如果找不到基础模板文件夹，则需要重新安装
- 			console.log( 'The basic template cannot be found. Try reinstall jugg please.' );
- 			process.exit( 1 );
- 		}
- 	} ) );
+  if( fs.exists( basic, function( exists ){
+    if( exists ){
+      fse.copySync( src, dest );
+      console.log( 'Blog ' + name + ' is created successfully.' );
+    }else{
+      // 如果找不到基础模板文件夹，则需要重新安装
+      console.log( 'The basic template cannot be found. Try reinstall jugg please.' );
+      process.exit( 1 );
+    }
+  } ) );
 }
--- bin/commands/server.js
+++ bin/commands/server.js
@@ -3,30 +3,30 @@
  */
 
 var _ 			= require( 'underscore' ),
-	connect 	= require( 'connect' ),
-	serveStatic = require('serve-static');
+    connect 	= require( 'connect' ),
+    serveStatic = require('serve-static');
 
 function _help(){
-	console.log( 'Usage: jugg server <name of your blog>' );
+  console.log( 'Usage: jugg server <name of your blog>' );
 }
 
 exports.exec = function( options ){
-	var port = 8001;
-	var name;
+  var port = 8001;
+  var name;
 
-	if( options.length ){
- 		name = options[ 0 ];
- 	}
- 	// 如果参数数组是空或不是数组，那么打印 hlep 并退出程序
- 	else{
- 		_help();
- 		return;
- 	}
+  if( options.length ){
+    name = options[ 0 ];
+  }
+  // 如果参数数组是空或不是数组，那么打印 hlep 并退出程序
+  else{
+    _help();
+    return;
+  }
 
- 	var app = connect();
+  var app = connect();
 
- 	app.use( serveStatic( __dirname + '/../../build/' + name ) );
- 	app.listen( port );
- 	console.log( 'Listening: http://localhost:' + port );
+  app.use( serveStatic( __dirname + '/../../build/' + name ) );
+  app.listen( port );
+  console.log( 'Listening: http://localhost:' + port );
 
 }
